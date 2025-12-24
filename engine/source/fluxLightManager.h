c0a0a679477749d875412de70f6400de5404e5a6
XXTH <tom@mxplus.de>
2025-12-24 17:24:47 +0100

    added light sources

---
+++ engine/source/fluxLightManager.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+#include "fluxLight.h"
+#include "fluxGlobals.h"
+
+// MAX_LIGHTS is defined in fluxGlobals.h now
+
+class FluxLightManager {
+private:
+    std::vector<FluxLight> mLights;
+
+public:
+    static FluxLightManager& getInstance() {
+        static FluxLightManager instance;
+        return instance;
+    }
+
+    FluxLightManager(const FluxLightManager&) = delete;
+    void operator=(const FluxLightManager&) = delete;
+
+    void addLight(const FluxLight& light) {
+        if (mLights.size() < MAX_LIGHTS) {
+            mLights.push_back(light);
+        } else {
+            // Log or handle error: too many lights
+            // For now, silently ignore or replace oldest light
+        }
+    }
+
+    void clearLights() {
+        mLights.clear();
+    }
+
+    const std::vector<FluxLight>& getLights() const {
+        return mLights;
+    }
+
+private:
+    FluxLightManager() {}
+};
+
+#define LightManager FluxLightManager::getInstance()
