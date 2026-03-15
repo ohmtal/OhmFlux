0aefaf8dba93e632d8eb99594acbb1ca7fb4f550
XXTH <tom@mxplus.de>
2026-03-16 00:51:09 +0100

    fixed memory issue in getpathes

--- engine/source/core/fluxGlobals.h
+++ engine/source/core/fluxGlobals.h
@@ -488,13 +488,21 @@ struct FluxSettings
     //     return  SDL_GetPrefPath(getSafeCompany().c_str(), getSafeCaption().c_str());
     // }
 
+    // std::string getPrefsPath() {
+    //     std::unique_ptr<char, void(*)(void*)> rawPath(
+    //         SDL_GetPrefPath(getSafeCompany().c_str(), getSafeCaption().c_str()),
+    //         SDL_free
+    //     );
+    //     return rawPath ? std::string(rawPath.get()) : "";
+    // }
     std::string getPrefsPath() {
-        std::unique_ptr<char, void(*)(void*)> rawPath(
-            SDL_GetPrefPath(getSafeCompany().c_str(), getSafeCaption().c_str()),
-            SDL_free
-        );
-        return rawPath ? std::string(rawPath.get()) : "";
+        static std::string cachedPath = "";
+        if (!cachedPath.empty()) return cachedPath;
+        const char* rawPath = SDL_GetPrefPath(getSafeCompany().c_str(), getSafeCaption().c_str());
+        cachedPath = rawPath ? std::string(rawPath) : "";
+        return cachedPath;
     }
+
 
     std::string getSafeCompany() {
         return fluxStr::sanitizeFilenameWithUnderScores(Company);
@@ -537,7 +545,7 @@ inline std::string getGamePath() {
      const char* rawPath = SDL_GetBasePath();
     if (rawPath) {
         cachedPath = rawPath;
-        SDL_free(const_cast<char*>(rawPath));
+        //SDL3 do NOT free! SDL_free(const_cast<char*>(rawPath));
     }
     return cachedPath;
 }
