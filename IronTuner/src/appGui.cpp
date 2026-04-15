ae896a500c62575777e139e7bf9a95de16a34f91
XXTH <tom@mxplus.de>
2026-04-15 16:30:03 +0200

    radio

--- IronTuner/src/appGui.cpp
+++ IronTuner/src/appGui.cpp
@@ -1382,6 +1382,7 @@ namespace IronTuner {
                     // if (ImGui::MenuItem("STOP")) Disconnect();
                     if (mAudioHandler->getPause())  { if (ImGui::MenuItem("RESUME")) mAudioHandler->setPause(false); }
                     else { if (ImGui::MenuItem("PAUSE")) mAudioHandler->setPause(true); }
+                    if (ImGui::MenuItem("FF 5sec"))  mAudioHandler->fastForward(44100 * 5);
                     bool disable =  (mAudioHandler->getNextTitle() == "" || FluxSchedule.isPending(mSkipToNextTitleTaskID));
                     if (disable) ImGui::BeginDisabled();
                     if (ImGui::MenuItem("FF to next title")) SkipToNextTitle();
@@ -2225,7 +2226,7 @@ namespace IronTuner {
         // 0.1 sec to let the ringbuffer update
         mSkipToNextTitleTaskID = FluxSchedule.add(0.1f, nullptr, [this]() {
             if (!mStreamHandler->isConnected()) return;
-            if (mAudioHandler->fastForward(44100)) {
+            if (mAudioHandler->fastForward(44100 * 3)) {
                 SkipToNextTitle();
             }
         });
