2f10ec988e1c3c8dd4a66ab82c087fc3acc34185
XXTH <tom@mxplus.de>
2026-06-27 02:40:35 +0200

    Elf/KorkTest

---
+++ ElfTest/assets/test.cs
@@ -0,0 +1,165 @@
+echo("Hello from Test.cs");
+//------------------------------
+if (!isObject(CleanupSet)) new SimSet(CleanupSet);
+else CleanupSet.deleteObjects();
+
+new GameCtrl(TEST) {};
+CleanupSet.add($Game);
+setVSync(false);
+//------------------------------
+function rl(%mode) {
+    exec("./test.cs");
+
+    if (%mode !$= "") {
+        $mode = %mode;
+        $i = $times = 0;
+    }
+}
+
+
+// benchmark
+$foo = new TestObj() {
+    TypeF32 bar = 0.0;
+};
+
+// $foo.dump();
+
+function setMy() {
+    $foo.myFloat = 2.2;
+}
+
+#define LOOP_COUNT 50000
+
+$mode = -1;
+
+function SpeedTest() {
+    %lF32 = 0.0;
+    %lS32 = 0;
+
+    //  --- count up a member without TYPEDEF ---
+    // .. object as global var
+    if ( $mode == 0 ) {
+        for (%i =0 ; %i < LOOP_COUNT; %i++) {
+            // $foo.bar += 0.1;
+        }
+    }
+    else
+    // .. object as local var
+    if ( $mode == 1 ) {
+        %foo = $foo;
+        for (%i =0 ; %i < LOOP_COUNT; %i++) {
+            %foo.bar += 0.1;
+        }
+    }
+    else
+    // .. object as local var + typed
+    if ( $mode == 2 ) {
+        %foo = $foo;
+        for (%i =0 ; %i < LOOP_COUNT; %i++) {
+            //NOTE Why is + - slower than * / ??
+
+            // %foo.myFloat = 0.1; // 65ms
+            // %foo.myFloat += 0.1; // 90ms
+            // %foo.myFloat *= 0.1; //70ms
+            // %foo.myFloat /= 0.1; //70ms
+            // %foo.myFloat -= 0.1; //90ms
+            %foo.myFloat =  %foo.myFloat * 0.1; //86ms
+        }
+    }
+    // ------ working on engine defined properties --------
+    else
+    // .. object as local var + set X
+    // NOTE: OMG is this slow !!!!!!!
+    if ( $mode == 3 ) {
+        %foo = $foo;
+        for (%i =0 ; %i < LOOP_COUNT; %i++) {
+            // THIS MUST BE A BUG !!! .x is slow like hell while myFloat is ok
+            // -- x,y,z,w,r,g,b,a are defined in parser ---
+            // 600 ms for this .... omg
+            // %foo.x += 0.1; //NOTE VERY VERY SLOW
+
+            // BUG testFloat is 600ms!!!
+            // so its not the .x
+            // %foo.testFloat *=   0.1;
+
+            //NOTE Verified its not my ELFSCRIPT_STRICT_SLOT_TYPE
+
+            // %testFloat = %foo.testFloat; //alone 60ms
+            // %testFloat *= 0.1;
+            // // !!!!!!!!!!!!1 THIS IT IS !!!!!!!!!!!!!!!!!!!!
+            // %foo.testFloat = 4.2;
+
+           // all three 87
+            %foo.testFloat += 0.1; // 67 ms now !!!
+
+            // %bar = %foo.testFloat * 1.001;
+            // %foo.testFloat = %bar;
+            //
+            // if (%bar > 10000.0) %foo.testFloat = 0.0;
+           //if ($i % 3 == 0)  echo(%foo.testFloat);
+
+        }
+    }
+    else
+    // .. object as local var + call walk
+    if ( $mode == 4 ) {
+        %foo = $foo;
+        for (%i =0 ; %i < LOOP_COUNT; %i++) {
+            %foo.walk(0.1, 0.4, 0.2); //NOTE FAST! 50-60ms
+        }
+    }
+    // .. object as local var + call getPos to a local var
+    if ( $mode == 5 ) {
+        %foo = $foo;
+        for (%i =0 ; %i < LOOP_COUNT; %i++) {
+            %posVec = %foo.getPos(); //not bad 111ms
+        }
+    }
+     // .. object as local var + call getx/y/z to a local vars
+    if ( $mode == 6 ) {
+        %foo = $foo;
+        for (%i =0 ; %i < LOOP_COUNT; %i++) {
+            // NOTE a bit slower than getPos: 122ms
+            %x = %foo.getX();
+            %foo.testFloat = %x;
+            // %y = %foo.getY();
+            // %z = %foo.getZ();
+        }
+    }
+    if ($mode == 7) {
+        $foo.testFloat += 0.1415  ;
+        %a = $foo.testFloat;
+        %a *= 1.001;
+        $foo.testFloat = %a;
+        // echo($foo.x);
+    }
+    if ($mode == 8) {
+        // $foo.testInt++;
+        for (%i =0 ; %i < LOOP_COUNT; %i++) {
+             // $foo.testInt += 1;
+             $foo.testInt = 1;
+             // $foo.testBool = 0;
+             // $foo.testString = "HUHU";
+             // %int = $foo.testInt;
+             // $foo.testSlot[1] ++;
+        }
+    }
+}
+
+
+$i = 0; $times = 0;
+
+function TEST::onRender(%this,%dt) {
+
+    if ($mode >= 0) {
+        $i++;
+        $times += GetFrameTime();
+        if ($i >= 10) {
+            $i -= 10;
+            echo("Speedtest MODE:" SPC $mode SPC "avg time:" SPC $times / 10 SPC "testfloat/int/bool:" SPC $foo.testFloat SPC $foo.testInt SPC $foo.testBool);
+            $times = 0;
+        }
+    }
+    SpeedTest();
+
+}
---
+++ KorkTest/src/test/testObj.cpp
@@ -0,0 +1,79 @@
+// // #include "console/engineAPI.h"
+#include "sim/simBase.h"
+#include "console/consoleTypes.h"
+
+    // class EmptyObject : public SimObject
+    // {
+    //     typedef SimObject Parent;
+    //
+    // public:
+    //     DECLARE_CONOBJECT(EmptyObject);
+    // };
+    // IMPLEMENT_CONOBJECT(EmptyObject);
+//--------------
+
+class TestObj: public SimObject
+{
+    typedef SimObject Parent;
+public:
+    DECLARE_CONOBJECT(TestObj);
+    F32 mX, mY, mZ;
+    F64 mDouble;
+    S32 mIndex;
+    bool mBool;
+    StringTableEntry mTestName;
+    S32 mSlots[3];
+
+    TestObj() {
+        mX = mY = mZ = 0.f;
+        mDouble = 0.f;
+        mIndex = 0;
+        mBool = false;
+        mTestName = StringTable->insert("NoName");
+        for (S32 i = 0; i < 3; i++) mSlots[i]=0;
+    }
+
+    static void initPersistFields()
+    {
+        // Parent::initPersistFields();
+        addField("x",     TypeF32,     Offset(mX, TestObj));
+        addField("y",     TypeF32,     Offset(mY, TestObj));
+        addField("z",     TypeF32,     Offset(mZ, TestObj));
+        addField("testFloat", TypeF32,   Offset(mDouble, TestObj));
+        addField("testInt",  TypeS32,    Offset(mIndex, TestObj));
+        addField("testBool", TypeBool,   Offset(mBool, TestObj));
+        addField("testString", TypeString, Offset(mTestName, TestObj));
+        addField("testSlot", TypeS32, Offset(mSlots, TestObj), 3); //WARNING count is not validated!!!
+
+    }
+
+    void setPos(F32 x, F32 y, F32 z) {
+        mX = x;
+        mY = y;
+        mZ = z;
+    }
+};
+
+IMPLEMENT_CONOBJECT(TestObj);
+
+// DefineEngineMethod(TestObj, getPos, String, (), , "get the position") {
+//     // we do nothing special here
+//     StringBuilder str;
+//     str.format("%g %g %g", object->mX, object->mY, object->mZ);
+//     return Con::getStringArg(str.end());
+// }
+//
+// DefineEngineMethod(TestObj, getX, F32, (), , "") {
+//   return object->mX;
+// }
+// DefineEngineMethod(TestObj, getY, F32, (), , "") {
+//     return object->mY;
+// }
+// DefineEngineMethod(TestObj, getZ, F32, (), , "") {
+//     return object->mZ;
+// }
+//
+// DefineEngineMethod(TestObj, walk, void, (F32 x, F32 y, F32 z), , "Set point where to walk to") {
+//     // we do nothing special here
+//     object->setPos(x,y,z);
+// }
